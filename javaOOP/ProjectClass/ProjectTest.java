e0fae939e64659406face8497b96e12a236c9783
Lana Theodory <lana.theodory@hotmail.com>
2021-06-10 13:59:03 +0300

    new

---
+++ javaOOP/ProjectClass/ProjectTest.java
@@ -0,0 +1,22 @@
+public class ProjectTest{
+public static void main(String[] args){
+    Project elevatorPitch = new Project("Lana" , "student" );
+    System.out.println(elevatorPitch.name);
+
+    // System.out.println(elevatorpitch.name +" , " + elevatorpitch.desc);
+
+    elevatorPitch.setName("ghazal");
+    elevatorPitch.setDesc("student");
+    
+    String X = elevatorPitch.getName();
+    String Y = elevatorPitch.getDesc();
+
+  
+
+    System.out.println( X +" , " + Y );
+
+
+
+}
+
+}
