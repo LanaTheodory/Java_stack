5995066a4db2163f687e1603d5f751c42e5ccd30
Lana Theodory <lana.theodory@hotmail.com>
2021-06-11 23:43:27 +0300

    updating assignment

--- JavaFun/Basic/Basic.java
+++ JavaFun/Basic/Basic.java
@@ -1,15 +1,20 @@
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class Basic{
-    public static int[] count(){
-        int[] newarray;
-        newarray = new int[255];
-        for (int i = 0; i < 255; i++){
-            newarray[i] = i+1;
 
-        }
-    return newarray;
-    }
+
+//     public static int[] count(){
+//         int[] newarray;
+//         newarray = new int[255];
+//         for (int i = 0; i < 255; i++){
+//             newarray[i] = i+1;
+
+//         }
+//     return newarray;
+//     }
+
+////////////////////////////////
 
 //  public static ArrayList countOdd(){
 //       ArrayList<Integer> myArray = new ArrayList<Integer>();
@@ -21,36 +26,137 @@ public class Basic{
     
 //         }
 //         return myArray;
+//  }
+// }
 
-
-
-    public static int countSum(){
-        int sum = 0;
+////////////////////////////////////////
+//     public static int countSum(){
+//         int sum = 0;
         
         
-        for (int c = 0; c < 255; c++){
-            sum += c;
-            System.out.println("new number :" + c + " " + "sum is:" + sum );
+//         for (int c = 0; c < 255; c++){
+//             sum += c;
+//             System.out.println("new number :" + c + " " + "sum is:" + sum );
 
-        }
-    return sum;
-    }
+//         }
+//     return sum;
+//     }
 
-    /////////////////////////////////////
-     public static int[] count(){
-        int[] newarray;
+//     /////////////////////////////////////
+//      public static int[] countArr(int[] newarray){
         
-        for (int i = 0; i < 255; i++){
-            newarray[i] = i+1;
+        
+//         for (int i = 0; i < newarray.length; i++){
+//             System.out.println(newarray[i]);
 
-        }
-    return newarray;
-    }
+//         }
+//     return newarray;
+//     }
 
+// /////////////////////////////////////////
+//    public static int maxArr(int[] newarray){
+//        int max = newarray[0];
+    
+        
+//         for (int i = 0; i < newarray.length ; i++){
+            
+//             if(max < newarray[i]  ){
+//                 max = newarray[i];
+//                 System.out.println(max);
+//             }
+           
 
-}
+//         }
+//     return max;
+//     }
 
+// /////////////////////////////////////////
+// // public static int avgArr(int[] newarray){
+// //        int sum = 0;
+        
+// //         for (int c = 0; c < newarray.length; c++){
+// //             sum += newarray[c];
+// //         }
+// //     int averagearr = sum/(newarray.length);
 
+// //     return averagearr;
+// //     }
+
+// //////////////////////////////////////
+// public static int greaterArr(int[] newarray , int y){
+//        int count = 0;
+    
+        
+//         for (int i = 0; i < newarray.length ; i++){
+            
+//             if(y < newarray[i]  ){
+//                 count ++;
+//             }
+           
+
+//         }
+//     return count;
+//     }
+
+// public static int[] doubleArr(int[] myArray){
     
 
+//     for(int i =0; i<myArray.length; i++){
+//         myArray[i] = (int) Math.pow(myArray[i], 2);
+//     }
+//     return  myArray;
+// }
+////////////////////////////////////////////////////
+
+// public static int[] posArr(int[] myArray){
+
+//     for(int i =0; i< myArray.length; i++){
+//         if(myArray[i] <0 ){
+//             myArray[i] = 0;
+//         }
+//     }
+//     return myArray;
+// }
+
+////////////////////////////////////////////////////
+//  public static int[] avgMaxMin(int[] newarray){
+//         int sum = 0;
+//         int max = newarray[0];
+//         int min = newarray[0];
+        
+//          for (int i = 0; i < newarray.length; i++){
+//             sum += newarray[i];
+
+//             if(max < newarray[i]){
+//                 max = newarray[i];
+                
+//             }
+//             if(min > newarray[i]){
+//                 min = newarray[i];
+//             }
+//          }
+//      int averagearr = sum/(newarray.length);
+//      int[] result = {max , min , averagearr};
+
+//      return result;
+//     }
+
+////////////////////////////////////////////////////
+// public static int[] shift(int[] myArray ){
+
+
+//     for(int i =0; i<myArray.length; i++){
+//         if(i != ( myArray.length -1)){
+        
+//         myArray[i] = myArray[i+1];
+//         } else {
+//             myArray[i] = 0;
+//         }
+        
+//     }
+//     return  myArray;
+ 
+// }
+ }    
+//////////////////////////////
    
--- JavaFun/Basic/BasicTest.java
+++ JavaFun/Basic/BasicTest.java
@@ -1,22 +1,69 @@
+import java.util.*;
+
 public class BasicTest{
     public static void main(String[] args){
-        Basic n = new Basic();
-        int[] N = n.count();
+        // Basic n = new Basic();
+        // int[] N = n.count();
     
         // ArrayList<Integer> myArray = new ArrayList<Integer>();
 
         // ArrayList myArray  = n.countOdd();
 
 
-        for ( int i= 0; i<N.length; i++){
-            System.out.println(N[i]);
-}  
+//         for ( int i= 0; i<N.length; i++){
+//             System.out.println(N[i]);
+// }  
 
     //    for ( int i= 0; i<myArray .size(); i++){
     //         System.out.println(M[i]);
     // }
         Basic f = new Basic();
-        int x = f.countSum();
+        // int x = f.countSum();
+
+        // ArrayList odd = f.countOdd();
+        // for(int i =0; i<odd.size(); i++){
+        //     System.out.println(odd.get(i));
+        // }
+
+        int[] myArray = {2,10,3};
+        // int maxar = f.maxArr(myArray);
+
+        // System.out.println(maxar);
+
+        // int avg = f.avgArr(myArray);
+        // System.out.println(avg);
+
+        // int greater = f.greaterArr(myArray , 1);
+        // System.out.println(greater);
+
+        // int[] doubarr = f.doubleArr(myArray);
+        // for(int i = 0; i < doubarr.length; i++){
+        //     System.out.println(doubarr[i]);
+
+        // }
+
+        // int[] shiftarr = f.shift(myArray);
+        //   for(int i = 0; i < shiftarr.length; i++){
+        //     System.out.println(shiftarr[i]);
+        // }
+
+        // int[] positive = f.posArr(myArray);
+        // for(int i = 0; i < positive.length; i++){
+        //    System.out.println(positive[i]);
+
+        // }
+        int[] result = f.avgMaxMin(myArray);
+          for(int i = 0; i < result.length; i++){
+           System.out.println(result[i]);
+
+        }
+   
+
+       
+
+
+
+
 
 }
 }
